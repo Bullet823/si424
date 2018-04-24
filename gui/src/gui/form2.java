89709310439859c201735ef43d4a4fe703d6f88b
Proyecto <Proyecto@C36-SERV2.upc.edu.pe>
2018-04-23 19:35:03 -0500

    Commit inicial

---
+++ gui/src/gui/form2.java
@@ -0,0 +1,107 @@
+package gui;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+
+public class form2 {
+
+	protected Shell shell;
+	private Text txtUrl;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			form2 window = new form2();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 370);
+		shell.setText("SWT Application");
+		
+		txtUrl = new Text(shell, SWT.BORDER);
+		txtUrl.setBounds(38, 10, 267, 21);
+		
+		Label lblUrl = new Label(shell, SWT.NONE);
+		lblUrl.setBounds(10, 13, 22, 15);
+		lblUrl.setText("url:");
+		
+		Browser browser = new Browser(shell, SWT.NONE);
+		browser.setBounds(10, 65, 414, 257);
+		
+		Button btnNewButton = new Button(shell, SWT.NONE);
+		btnNewButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				browser.setUrl(txtUrl.getText());
+				
+			}
+		});
+		btnNewButton.setBounds(20, 34, 75, 25);
+		btnNewButton.setText("Ir");
+		
+		Button btnNewButton_1 = new Button(shell, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				browser.refresh();
+			}
+		});
+		btnNewButton_1.setBounds(101, 34, 75, 25);
+		btnNewButton_1.setText("Actualizar");
+		
+		Button btnNewButton_2 = new Button(shell, SWT.NONE);
+		btnNewButton_2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				browser.back();
+			}
+		});
+		btnNewButton_2.setBounds(182, 37, 75, 25);
+		btnNewButton_2.setText("Retroceder");
+		
+		Button btnNewButton_3 = new Button(shell, SWT.NONE);
+		btnNewButton_3.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			browser.forward();
+			
+			}
+		});
+		btnNewButton_3.setBounds(272, 37, 75, 25);
+		btnNewButton_3.setText("Avanzar");
+
+	}
+}
